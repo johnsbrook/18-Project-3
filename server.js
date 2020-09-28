2af33bbba344bd5e85a171cbe771c4ff787435a4
ivanzapatarivera <falenyo@gmail.com>
2020-09-28 12:54:00 -0300

    created api per category

--- server.js
+++ server.js
@@ -42,8 +42,85 @@ app.get('/api/products', function (req, res) {
     })
 })
 
-app.get('/api/products/category', function (req, res) {
+app.get('/api/products/bagel', function (req, res) {
   db.find({category: "bagel"})
+  .sort({product: 1})
+  .then(cb => {
+      res.json(cb);
+  })
+  .catch(err => {
+      res.status(400).json(err);
+  })
+})
+
+app.get('/api/products/breakfast', function (req, res) {
+  db.find({category: "breakfast"})
+  .sort({product: 1})
+  .then(cb => {
+      res.json(cb);
+  })
+  .catch(err => {
+      res.status(400).json(err);
+  })
+})
+
+app.get('/api/products/coffee', function (req, res) {
+  db.find({category: "coffee"})
+  .sort({product: 1})
+  .then(cb => {
+      res.json(cb);
+  })
+  .catch(err => {
+      res.status(400).json(err);
+  })
+})
+
+app.get('/api/products/pastry', function (req, res) {
+  db.find({category: "pastry"})
+  .sort({product: 1})
+  .then(cb => {
+      res.json(cb);
+  })
+  .catch(err => {
+      res.status(400).json(err);
+  })
+})
+
+app.get('/api/products/promo', function (req, res) {
+  db.find({category: "promo"})
+  .sort({product: 1})
+  .then(cb => {
+      res.json(cb);
+  })
+  .catch(err => {
+      res.status(400).json(err);
+  })
+})
+
+app.get('/api/products/sandwiches', function (req, res) {
+  db.find({category: "sandwiches"})
+  .sort({product: 1})
+  .then(cb => {
+      res.json(cb);
+  })
+  .catch(err => {
+      res.status(400).json(err);
+  })
+})
+
+app.get('/api/products/sides', function (req, res) {
+  db.find({category: "sides"})
+  .sort({product: 1})
+  .then(cb => {
+      res.json(cb);
+  })
+  .catch(err => {
+      res.status(400).json(err);
+  })
+})
+
+app.get('/api/products/tea', function (req, res) {
+  db.find({category: "tea"})
   .sort({product: 1})
   .then(cb => {
       res.json(cb);
