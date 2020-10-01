20cfd2013d1f15e785151a6ffa1a33ae31f7ed45
ivanzapatarivera <falenyo@gmail.com>
2020-10-01 04:22:15 -0400

    placed event listener functionality into order.js

--- public/selections.js
+++ public/selections.js
@@ -10,6 +10,7 @@ var idNo = 0;
 
 products();
 
+
 function products() {
   // Appending promo items with fetch and map
   const promo = document.querySelector(".promo");
@@ -314,28 +315,4 @@ function products() {
     });
 }
 
-// $(document).on('click','.add',function(){alert("this")})
-var amount = [];
-document.body.addEventListener("click", function (event) {
-  if (event.target.className == "add") {
-    console.log(event.target.parentElement.children[1].outerText);
-    var item = event.target.parentElement.children[1].outerText;
-    item = item.split("$");
-    console.log(item);
-    amount.push(item[1]);
-    $(".item").append(item[0] + "<br>");
-    $(".amount").append("$ " + item[1] + "<br>");
-  }
-  console.log(amount);
-  amount = amount.map(Number);
-  console.log(amount);
 
-  var sum = amount.reduce(function (a, b) {
-    return a + b;
-  }, 0);
-
-  console.log(sum);
-  var total = sum.toFixed(2);
-
-  document.getElementById("total").innerHTML = "$ " + total;
-});
