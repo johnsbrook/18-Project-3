1ee6c8237c2b4d43761b83f5827892cf4df8d9be
ivanzapatarivera <falenyo@gmail.com>
2020-10-02 21:54:14 -0400

    created bagel

--- public/selections.js
+++ public/selections.js
@@ -315,4 +315,46 @@ function products() {
     });
 }
 
+// Appending breakfast using fetch and map
+const elBagel = document.querySelector(".bagel");
+const uBagel = "https://coffeeproducts.herokuapp.com/api/products/bagel";
+fetch(uBagel)
+  .then((res) => res.json(uBagel))
+  .then((data) => {
+    data
+      .map((d) => {
+        console.log(d);
+        let li = createNode("li");
+        btn = createNode("button");
+        btn2 = createNode("button");
+        (br = createNode("br")), (spanH = createNode("div"));
+        span = createNode("span");
+        p = createNode("div");
+        image = createNode("img");
 
+        spanH.innerText = d.product + " " + " $" + d.price;
+        spanH.id = d._id;
+        spanH.setAttribute("class", "add");
+        p.innerText = d.description;
+        (btn.innerText = "Add"), btn.setAttribute("class", "add");
+        btn.setAttribute("id", d.category + "[" + (idNo += idNo) + "]");
+        btn2.innerText = "More";
+        image.setAttribute("src", d.image);
+        image.setAttribute(
+          "style",
+          "width: 150px; height: auto; border-radius: 15px;"
+        );
+
+        appendNode(li, image);
+        appendNode(li, spanH);
+        appendNode(li, span);
+        appendNode(li, p);
+        appendNode(li, btn);
+        appendNode(li, btn2);
+        appendNode(elSides, li);
+      })
+      .catch((err) => {
+        console.error("Error: ", err);
+      });
+  });
+}
