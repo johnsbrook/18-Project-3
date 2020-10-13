2a8858d3ef31041e3683dcbb03751ffbe48bba02
Ivan Zapata <falenyo@gmail.com>
2020-10-12 23:29:56 -0400

    changed seeds

--- Seeders/seed.js
+++ Seeders/seed.js
@@ -14,11 +14,40 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Coffeeshop", {
       category: "Coffee",
       description: "Bold brewed black coffee",
       image: "http://www.google.com"
+    }, 
+    {
+      product: "Espresso Latte",
+      price: 2.99,
+      category: "Coffee",
+      description: "Bold brewed black coffee",
+      image: "http://www.google.com"
     }
 
   ];
+
+  let checkout = [
+    {
+      product: "Espresso",
+      price: 1.99
+    },
+    {
+      product: "Espresso Latte",
+      price: 2.99
+    }
+  ]
   
 db.Coffeeshop.deleteMany({})
+  .then(() => db.Coffeeshop.collection.insertMany(checkout))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+  db.Coffeeshop.deleteMany({})
   .then(() => db.Coffeeshop.collection.insertMany(productSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
